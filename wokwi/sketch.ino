937213f1e5ddcb5dd5bd5b65b127f595b03d823f
benjaminmoyano444 <moyanobenja004@gmail.com>
2025-11-14 18:35:50 -0300

    Add files via upload

---
+++ wokwi/sketch.ino
@@ -0,0 +1,20 @@
+
+int led1 = 8;
+int led2 = 9;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(led1, OUTPUT);
+pinMode(led2, OUTPUT);
+}
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(led1, HIGH);
+digitalWrite(led2, LOW);
+delay(1000);
+digitalWrite(led1, LOW);
+digitalWrite(led2, HIGH);
+delay(1000);
+}
